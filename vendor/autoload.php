86dcc0b90cd9da8232d411634c6c87b2b15bd708
Aleksandr Prykhodko <oprykhodko@bybondar.com>
2018-07-17 14:10:07 +0300

    Added composer and configs

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitDrupal8::getLoader();
+return ComposerAutoloaderInitb63d0d75127da98fbd7196298cb35a0d::getLoader();
